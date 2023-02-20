b805d7a6ea836864d47805156e8b2b787cb75182
test <tobias.wedhorn@gmx.de>
2023-02-20 16:19:45 +0100

    displayed full range scall

--- src/components/Stave.jsx
+++ src/components/Stave.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 
 function Stave() {
   let keySignature = "C";
-//   let abcString = "X:1<br/>L:1/1<br/>M:<br/>K:C<br/>V:V:V1 clef=treble<br/>G,A,B,CDEFGABcdefgabc'"
+  let abcString = "X:1\nL:1/1\nM:\nK:C\nV:V:V1 clef=treble\nG,A,B,CDEFGABcdefgabc'"
 //   let abcString =`X:1\nK:${keySignature}\nDD AA|BBA2|\n`;
 
   useEffect(() => {
