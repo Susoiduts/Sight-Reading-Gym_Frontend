2839fd622d526268507409b17f4935204ebd912a
test <tobias.wedhorn@gmx.de>
2023-03-07 14:35:13 +0100

    added feedback seciton

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Feedback() {
-  return <div>Feedback</div>;
+return (
+<div style={{textAlign: "center", marginTop: "20px"}}>
+There is a feature you would like to see?<br />
+Share your ideas and feedback here on the board: 
+<p><a href="https://sightsinggym-feedback-9kgdvvab.featureupvote.com/">Board</a></p>
+</div>
+);
 }
 
 export default Feedback;
